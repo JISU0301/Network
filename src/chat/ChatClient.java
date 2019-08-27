95dade09cc56dc2f87c5663c202970e3e92d04cc
JISU0301 <53724526+JISU0301@users.noreply.github.com>
2019-08-27 17:55:09 +0900

    Update ChatClient.java

--- src/chat/ChatClient.java
+++ src/chat/ChatClient.java
@@ -5,19 +5,18 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.net.ConnectException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.util.Scanner;
 
-import chat.client.win.ChatWindow;
 
 public class ChatClient {
-	private static final String SERVER_IP = "192.168.1.7";
+	private static final String SERVER_IP = "192.168.56.1";
 	private static final int SERVER_PORT = 9999;
 
 	public static void main(String[] args) {
 		Socket socket = null;
-		String name = null;
 		Scanner scanner = null;
 
 		try {
@@ -27,6 +26,7 @@ public class ChatClient {
 			socket = new Socket();
 			
 			socket.connect(new InetSocketAddress(SERVER_IP, SERVER_PORT));
+			log("connected");
 			
 			PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF-8" ), true );
 			BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
@@ -36,21 +36,31 @@ public class ChatClient {
 			pw.println("join:" + nickname);
 			pw.flush();
 			
+			String ack = br.readLine();
+			if("join:OK".equals(ack)) {
+				System.out.println("입장하였습니다. 즐거운 채팅 되세요");
+			}
+			
 			new ChatClientThread(socket).start();
 		
 			while( true ) {
-				System.out.print(">>");
+				if(scanner.hasNextLine() == false) {
+					continue;
+				}
+				
 				String input = scanner.nextLine();
 
-				if ("quit".equals(input) == true ) {
+				if ("quit".equals(input)) {
 					pw.println("quit:" + nickname);
 					break;
-				}else {
-					pw.println( "MESSAGE:" + input );
+				}
+				if ("".equals(input) == false) {
+					pw.println( "message:" + input );
 				}
 			}
-
-		}catch(IOException e) {
+		}catch(ConnectException e) {
+			log("서버[" + SERVER_IP + ":" + SERVER_PORT + "]에 연결할 수 없습니다.");
+		}catch(Exception e) {
 			log("error : " + e);
 		}finally { 
 			try {
@@ -60,13 +70,13 @@ public class ChatClient {
 					socket.close();
 				}
 			}catch(IOException e) {
-				
+				log("error : " + e);
 			}
 		}
 	}
 
 	public static void log(String log) {
-		System.out.println("[Server]" + log);
+		System.out.println("[Client Server]" + log);
 		
 	}
 }
