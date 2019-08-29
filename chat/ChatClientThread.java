7acd6bb54993b4558011e08816331bed138a635c
JISU0301 <53724526+JISU0301@users.noreply.github.com>
2019-08-29 09:52:17 +0900

    Update ChatClientThread.java

--- chat/ChatClientThread.java
+++ chat/ChatClientThread.java
@@ -3,21 +3,27 @@ package chat;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.Socket;
 import java.net.SocketException;
 
-public class ChatClientThread extends Thread {
+
+public class ChatClientThread extends Thread{
+
 	private Socket socket;
 	
-	public ChatClientThread( Socket socket ) {
+	public ChatClientThread(Socket socket) {
 		this.socket = socket;
 	}
 	
+	
 	@Override
 	public void run() {
-		try{
-			BufferedReader br = new BufferedReader( new InputStreamReader(socket.getInputStream(), "UTF-8" ) );
-
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
+			
 			while( true ) {
 				String message = br.readLine();
 				if( message == null ) {
@@ -25,12 +31,18 @@ public class ChatClientThread extends Thread {
 				}
 				System.out.println( message );
 			}
-		} catch( SocketException ex ){
-			ChatClient.consoleLog( "" + ex );	
-		} catch( IOException ex ){
-			ChatClient.consoleLog( "다음 이유로 프로그램을 종료 합니다 :" + ex );	
-		} finally {
+			
+		}catch( SocketException ex ){
+			ChatClient.log( "" + ex );	
+		} catch (IOException ex) {
+			ChatClient.log("IOException" +ex);
+		}finally {
 			System.exit(0);
 		}
+		
+		
 	}
+
+	
+	
 }
